5b329b0e01e30e0bf158d0abc383f549a350f6d5
zaoqi <zaomir@outlook.com>
2018-08-05 16:28:16 +0800

    .

--- example-void.c
+++ example-void.c
@@ -1,4 +1,11 @@
 #include "module<"
+#include "require<"
+#include "require<"
+#include "require<"
+#include "example-global-define.c"
+#include ">require"
+#include ">require"
+#include ">require"
 define_public_inline_function(void mkvoid2())({
 	make_void();
 })
