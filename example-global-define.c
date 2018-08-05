f8ad270c446d33395fa9c5d328a51e1dd810fe4f
zaoqi <zaomir@outlook.com>
2018-08-05 16:12:23 +0800

    .

---
+++ example-global-define.c
@@ -0,0 +1,3 @@
+#include "module<"
+define_private_zero(int, zero_int);
+#include ">module"
---
+++ example-void.c
@@ -0,0 +1,5 @@
+#include "module<"
+define_public_inline_function(void mkvoid2())({
+	make_void();
+})
+#include ">module"
