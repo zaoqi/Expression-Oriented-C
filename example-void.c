927554ab1b2c6f759e255e667d07d7062f8db9f4
zaoqi <zaomir@outlook.com>
2018-08-09 21:55:28 +0800

    .

--- example-void.c
+++ example-void.c
@@ -3,3 +3,5 @@ define_public_lambda(makevoid2, void)({
 	make_void();
 })
 #include ">module"
+EOC_HELPER_tail(sd,fa,df,adf,ad,fas,df,0)
+EOC_HELPER_if(EOC_HELPER_zero_p(EOC_HELPER_count()),ha,1)
--- make_macro_tools.sh
+++ make_macro_tools.sh
@@ -22,6 +22,16 @@ fi
 
 max=64
 
+from_to(){
+	eval echo {$1..$2}
+}
+prefix_from_to(){
+	eval echo $1{$2..$3}
+}
+prefix_from_to_suffix(){
+	eval echo $1{$2..$3}$4
+}
+
 define(){
 	echo "#define $prefix$*"
 }
@@ -50,6 +60,19 @@ defn(){
 		fi
 	done
 }
+add_between(){
+	local x="$1"
+	shift
+	echo -n "$1"
+	shift
+	while [ -n "$1" ]
+	do
+		echo -n "$x"
+		echo -n "$1"
+		shift
+	done
+	echo
+}
 call(){
 	echo -n "$prefix$1("
 	shift
@@ -70,29 +93,62 @@ call(){
 S(){
 	echo "$prefix$*"
 }
-from_to(){
-	eval echo {$1..$2}
+symbol_append(){
+	if [ -z "$2" ]
+	then
+		echo "$1"
+	else
+		local x="$1"
+		shift
+		echo $(call symbol_append_with_macro "$x" $(symbol_append "$@"))
+	fi
 }
-prefix_from_to(){
-	eval echo $1{$2..$3}
+expand(){
+	call expand "$*"
 }
-prefix_from_to_suffix(){
-	eval echo $1{$2..$3}$4
+ifdef(){
+	echo "#ifdef $*"
+}
+el(){
+	echo else
+}
+endif(){
+	echo endif
 }
 defn error x "{{{!!!ERROR x ERROR!!!}}}"
 defn expand x x
 defn count_helper0 $(prefix_from_to _ 0 $max) x ... x
-defn count_helper1 ... $(call expand $(call count_helper0 __VA_ARGS__ $(from_to $max 0)))
+defn count_helper1 ... $(expand $(call count_helper0 __VA_ARGS__ $(from_to $max 0)))
 defn count ... $(call count_helper1 _Nothing '##__VA_ARGS__')
 echo '#define EOC_HELPER_count_assert0()
 #define EOC_HELPER_count_assert1() EOC_HELPER_error("this compiler does not offers an extension that allows ## to appear after a comma and before __VA_ARGS__ , in which case the ## does nothing when __VA_ARGS__ is non-empty, but removes the comma when __VA_ARGS__ is empty")
 EOC_HELPER_expand(EOC_HELPER_symbol_append_with_macro(EOC_HELPER_count_assert, EOC_HELPER_count()) ())'
 defn symbol_append x y "x##y"
 defn symbol_append_with_macro x y $(call symbol_append x y)
-defn with_count ider ... $(call expand $(call symbol_append_with_macro ider $(call count __VA_ARGS__))'(__VA_ARGS__)')
+defn with_count ider ... $(expand $(symbol_append ider $(call count __VA_ARGS__))'(__VA_ARGS__)')
+
+defn if b x y $(expand $(symbol_append $(S if) b)'(x,y)')
+defn if1 x y x
+defn iftrue x y x
+defn if0 x y y
+defn iffalse x y y
 
 defn tail ... $(call with_count $(S tail) __VA_ARGS__)
 for i in $(from_to 1 $max)
 do
 	defn tail$i $(prefix_from_to _ 1 $i) _$i
+done
+
+defn init ... $(call with_count $(S init) __VA_ARGS__)
+for i in $(from_to 1 $max)
+do
+	let isub=i-1
+	defn init$i $(prefix_from_to _ 1 $i) $(add_between , $(prefix_from_to _ 1 $isub))
+done
+
+defn zero_p x $(symbol_append $(S zero_p) x)
+define zero_p0 true
+for i in $(from_to 1 $max)
+do
+	define zero_p$i false
 done
